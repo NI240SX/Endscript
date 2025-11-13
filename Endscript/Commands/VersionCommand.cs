4bb740b430fe27aee41a72e5c746d88acfecfa5c
needeka <nis240sx89@gmail.com>
2025-11-13 15:15:00 +0100

    added support for SkinRegion data + bypass of the Collection Editor changed the program name and altered the "version" command to prevent loading scripts made for this fork in others

--- Endscript/Commands/VersionCommand.cs
+++ Endscript/Commands/VersionCommand.cs
@@ -18,9 +18,12 @@ namespace Endscript.Commands
 
 		public override void Prepare(string[] splits)
 		{
-			if (splits.Length != 2) throw new InvalidArgsNumberException(splits.Length, 2);
+			if (splits.Length != 2 && splits.Length != 3) throw new InvalidArgsNumberException(splits.Length, 2, 3);
 
 			this.CheckValidVersion(splits[1]);
+
+			// add data in version to prevent Binercover-only scripts to be loaded by concurrent forks
+			if (splits.Length == 3) if (splits[2].ToLower() != "binercover") throw new Exception($"Endscript version declares the extension {splits[2]} ; this fork only supports Binercover.");
 
 			if (Version.Value.CompareTo(this._version) >= 0) return;
 			else throw new Exception($"Endscript version {this._version} is higher than executable {Version.Value}");
